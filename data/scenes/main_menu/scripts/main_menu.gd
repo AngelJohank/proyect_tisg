f7cc8a049e0809a19d2f378c8798087cf030d309
AngelJohank <74012324+AngelJohank@users.noreply.github.com>
2021-02-21 00:08:32 -0500

    Add files via upload

--- data/scenes/main_menu/scripts/main_menu.gd
+++ data/scenes/main_menu/scripts/main_menu.gd
@@ -3,13 +3,15 @@ extends Node2D
 func _ready():
 	$background/background_animation.play("main")
 
-
 func _on_config_button_pressed():
-	$configurations.visible = !$configurations.visible
+	$configuration.visible = !$configuration.visible
 	$text.visible = !$text.visible
-
-
+	if $text.visible == false:
+		$configuration/papyrus/AnimationPlayer.play("papyrus")
+		$configuration/conftext/conftext_animation.play("shake")
+	else:
+		$configuration/papyrus/AnimationPlayer.stop()
+		$configuration/conftext/conftext_animation.stop()
 
 func _on_background_audio_finished():
 	$background_audio.play()
-
